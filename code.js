b7e7f481000b808bb642aa0a642b30ff364a3612
Satish-Kumar15 <87117247+Satish-Kumar15@users.noreply.github.com>
2021-07-12 18:36:41 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,21 @@
+function countPairsBruteForce(X, Y,Z, m, n){
+
+    let ans = 0;
+
+    for(let i=0; i<m; i++ ){ 
+
+        for(let j=0;j<n;j++){ 
+
+            if ((Math.pow(X[i], 2) + Math.pow(Y[j], 2)==Math.pow(Z[i],2))){
+
+                ans += 1;
+
+             }
+
+        }
+
+    }
+
+    return ans;
+}
+ 
